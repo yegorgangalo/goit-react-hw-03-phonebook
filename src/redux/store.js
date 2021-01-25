a4ec321aba8a7f1239db6b04b62a66a88ed6d20d
Yegor Gangalo <gangaloyegor@gmail.com>
2021-01-26 01:24:00 +0200

    begin refactoring

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,18 @@
-import { createStore, combineReducers } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { configureStore } from '@reduxjs/toolkit';
+// import { createStore, combineReducers } from 'redux';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 import contactsReducer from './contacts/contacts-reducer';
 
-const rootReducer = combineReducers({
-  contacts: contactsReducer,
+// const rootReducer = combineReducers({
+//   contacts: contactsReducer,
+// });
+
+// const store = createStore(rootReducer, composeWithDevTools());
+const store = configureStore({
+  reducer: { contacts: contactsReducer },
+  devTools: process.env.NODE_ENV === 'development',
 });
 
-const store = createStore(rootReducer, composeWithDevTools());
 export default store;
 
 // const initialState = {
