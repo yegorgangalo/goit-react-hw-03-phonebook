45e06a59d97e7d53ebb4c030c1d5ed8c44e920dc
Yegor Gangalo <gangaloyegor@gmail.com>
2021-01-30 00:06:05 +0200

    back to combineReducers, createReducer

--- src/redux/contacts/contacts-reducer.js
+++ src/redux/contacts/contacts-reducer.js
@@ -1,5 +1,5 @@
-// import { combineReducers, createReducer } from '@reduxjs/toolkit';
-import { createSlice } from '@reduxjs/toolkit';
+// import { createSlice } from '@reduxjs/toolkit';
+import { combineReducers, createReducer } from '@reduxjs/toolkit';
 import * as actions from './contacts-actions';
 import * as operations from './contacts-operations';
 const { changeFilter, editContact } = actions;
@@ -15,98 +15,95 @@ const initialState = [
     {id: 'id-6', name: 'Ed Clirence', number: '217-01-46', experience: 'middle', skills: ['HTML', 'CSS', 'JS']},
 ];
 /* ---------------------extraReducersOBJECTs---------------------------- */
-const extraReducersFulfilledObj = {
-    [addContact.fulfilled]: (state, { payload }) => {
-        state.items.push(payload);
-        state.loading = !state.loading;
-    },
-    [deleteContact.fulfilled]: (state, { payload }) => {
-        state.items = state.items.filter(({ id }) => id !== payload);
-        state.loading = !state.loading;
-    },
-    [patchContact.fulfilled]: (state, { payload }) => {
-        state.items = state.items.map(item => item.id === payload.id ? payload : item);
-        state.loading = !state.loading;
-        state.editItem = null;
-    },
-    [fetchContacts.fulfilled]: (state, { payload }) => {
-        state.items.push(...payload);
-        state.loading = !state.loading;
-    },
-    [changeFilter]: ( state , { payload }) => {state.filter=payload.toLowerCase()},
-    [editContact]: (state, { payload }) => { state.editItem = payload },
-  }
+// const extraReducersFulfilledObj = {
+//     [addContact.fulfilled]: (state, { payload }) => {
+//         state.items.push(payload);
+//         state.loading = !state.loading;
+//     },
+//     [deleteContact.fulfilled]: (state, { payload }) => {
+//         state.items = state.items.filter(({ id }) => id !== payload);
+//         state.loading = !state.loading;
+//     },
+//     [patchContact.fulfilled]: (state, { payload }) => {
+//         state.items = state.items.map(item => item.id === payload.id ? payload : item);
+//         state.loading = !state.loading;
+//         state.editItem = null;
+//     },
+//     [fetchContacts.fulfilled]: (state, { payload }) => {
+//         state.items.push(...payload);
+//         state.loading = !state.loading;
+//     },
+//     [changeFilter]: ( state , { payload }) => {state.filter=payload.toLowerCase()},
+//     [editContact]: (state, { payload }) => { state.editItem = payload },
+//   }
 
-const setErrorToggleLoading = (state, { payload }) => {
-    const { status,config,request,statusText} = payload;
-    state.error=`Error ${status}. Can't ${config.method} by ${request.responseURL}. ${statusText}`;
-    state.loading = !state.loading;
-        };
-const resetErrorToggleLoading = (state) => {
-    state.error = null;
-    state.loading = !state.loading;
-};
-const extraReducersRejectedPendingObj = Object.values(operations)
-    .reduce((accObj, operation) =>
-        ({ ...accObj, [operation.rejected]: setErrorToggleLoading, [operation.pending]: resetErrorToggleLoading }), {});
+// const setErrorToggleLoading = (state, { payload }) => {
+//     const { status,config,request,statusText} = payload;
+//     state.error=`Error ${status}. Can't ${config.method} by ${request.responseURL}. ${statusText}`;
+//     state.loading = !state.loading;
+// };
+// const resetErrorToggleLoading = (state) => {
+//     state.error = null;
+//     state.loading = !state.loading;
+// };
+// const extraReducersRejectedPendingObj = Object.values(operations)
+//     .reduce((accObj, operation) =>
+//         ({ ...accObj, [operation.rejected]: setErrorToggleLoading, [operation.pending]: resetErrorToggleLoading }), {});
 
-/* ------------------------------------------------- */
-const contactsSlice = createSlice({
-  name: 'contacts',
-    initialState: { items: initialState, filter: '', loading: false, error: null, editItem: null },
-    reducers: {
+// /* ---------------------createSlice---------------------------- */
+// const contactsSlice = createSlice({
+//     name: 'contacts',
+//     initialState: { items: initialState, filter: '', loading: false, error: null, editItem: null },
+//     extraReducers: {...extraReducersFulfilledObj, ...extraReducersRejectedPendingObj},
+// });
 
-    },
-  extraReducers: {...extraReducersFulfilledObj, ...extraReducersRejectedPendingObj},
-});
-
-export default contactsSlice.reducer;
+// export default contactsSlice.reducer;
 
 /* ---------------ITEMS_REDUCER---------------------- */
-// const items = createReducer(initialState, {
-//     [addContact.fulfilled]: (state, { payload }) => [...state, payload],
-//     [deleteContact.fulfilled]: (state, { payload }) => state.filter(({ id }) => id !== payload),
-//     [patchContact.fulfilled]: (state, { payload }) => state.map(contact => contact.id === payload.id ? payload : contact),
-//     [fetchContacts.fulfilled]: (state, { payload }) => payload.length>3 ? payload : [...state, ...payload],
-//     // [fetchContacts.fulfilled]: (state, { payload }) => payload,
-// });
+const items = createReducer(initialState, {
+    [addContact.fulfilled]: (state, { payload }) => [...state, payload],
+    [deleteContact.fulfilled]: (state, { payload }) => state.filter(({ id }) => id !== payload),
+    [patchContact.fulfilled]: (state, { payload }) => state.map(contact => contact.id === payload.id ? payload : contact),
+    [fetchContacts.fulfilled]: (state, { payload }) => payload.length>3 ? payload : [...state, ...payload],
+    // [fetchContacts.fulfilled]: (state, { payload }) => payload,
+});
 
-// /* ---------------FILTER_REDUCER---------------------- */
-// const filter = createReducer('', {
-//     [changeFilter]: ( _ , { payload }) => payload.toLowerCase(),
-// })
+/* ---------------FILTER_REDUCER---------------------- */
+const filter = createReducer('', {
+    [changeFilter]: ( _ , { payload }) => payload.toLowerCase(),
+})
 
-// /* ---------------EDITCONTACT_REDUCER---------------------- */
-// const editItem = createReducer(null, {
-//     [editContact]: (_, { payload }) => payload,
-//     [patchContact.fulfilled]: () => null,
-// })
+/* ---------------EDITCONTACT_REDUCER---------------------- */
+const editItem = createReducer(null, {
+    [editContact]: (_, { payload }) => payload,
+    [patchContact.fulfilled]: () => null,
+})
 
-// /* ---------------LOAD_REDUCER---------------------- */
-// const toggleLoading = (state) => !state;
-// const reducerLoadingObj = Object.values(operations)
-//     .reduce((accObj, operation) =>
-//         ({ ...accObj, [operation.fulfilled]: toggleLoading, [operation.rejected]: toggleLoading, [operation.pending]: toggleLoading }), {});
-// const loading = createReducer(false, reducerLoadingObj);
+/* ---------------LOAD_REDUCER---------------------- */
+const toggleLoading = (state) => !state;
+const reducerLoadingObj = Object.values(operations)
+    .reduce((accObj, operation) =>
+        ({ ...accObj, [operation.fulfilled]: toggleLoading, [operation.rejected]: toggleLoading, [operation.pending]: toggleLoading }), {});
+const loading = createReducer(false, reducerLoadingObj);
 
-// /* ---------------ERROR_REDUCER---------------------- */
-// const reducerErrorObj = Object.values(operations)
-//     .reduce((accObj, operation) => {
-//         // const setError = (_, { error }) => error;
-//         const setError = (_, { payload }) => {
-//             const { status,config,request,statusText} = payload;
-//             return `Error ${status}. Can't ${config.method} by ${request.responseURL}. ${statusText}`;
-//         };
-//         const resetError = () => null;
-//         return ({ ...accObj, [operation.rejected]: setError, [operation.pending]: resetError });
-//     }, {});
-// const error = createReducer(null, reducerErrorObj);
-// /* ---------------------------------------------------- */
+/* ---------------ERROR_REDUCER---------------------- */
+const reducerErrorObj = Object.values(operations)
+    .reduce((accObj, operation) => {
+        // const setError = (_, { error }) => error;
+        const setError = (_, { payload }) => {
+            const { status,config,request,statusText} = payload;
+            return `Error ${status}. Can't ${config.method} by ${request.responseURL}. ${statusText}`;
+        };
+        const resetError = () => null;
+        return ({ ...accObj, [operation.rejected]: setError, [operation.pending]: resetError });
+    }, {});
+const error = createReducer(null, reducerErrorObj);
+/* ---------------------------------------------------- */
 
-// export default combineReducers({
-//   items,
-//   filter,
-//   loading,
-//   error,
-//   editItem,
-// });
+export default combineReducers({
+  items,
+  filter,
+  loading,
+  error,
+  editItem,
+});
